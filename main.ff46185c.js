44ef0495b46abf1b4827fc2e867cef0e5f75f615
Artur <hafud@mail.ru>
2019-08-21 22:26:45 +0400

    update path

--- main.ff46185c.js
+++ main.ff46185c.js
@@ -303,7 +303,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46291" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7863" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
